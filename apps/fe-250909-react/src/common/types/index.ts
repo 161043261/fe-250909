657aa1bb2f6cdc00c2adb142f90acb6e59500fa9
hangtiancheng <hangtiancheng@bytedance.com>
2025-09-12 16:02:57 +0800

    feat: Introduce new features

--- apps/fe-250909-react/src/common/types/index.ts
+++ apps/fe-250909-react/src/common/types/index.ts
@@ -9,3 +9,14 @@ export interface IAppSettings {
 }
 
 export type TMainWindowMsg = 'close' | 'esc' | 'maximize' | 'minimize' | 'reload'
+
+export type TServiceName = 'achievement' | 'config' | 'gacha' | 'settings' | 'fps' | 'update'
+
+export type TApi = {
+  sendMainWindowMsg: (msg: TMainWindowMsg) => void
+  invokeReadJsonFile: (filename: string) => Promise<unknown>
+} & {
+  [serviceName in TServiceName]?: {
+    [fnName: string]: (...args: unknown[]) => Promise<unknown>
+  }
+}
--- apps/fe-250909-react/src/preload/index.ts
+++ apps/fe-250909-react/src/preload/index.ts
@@ -1,16 +1,38 @@
-import { contextBridge } from 'electron'
+import { contextBridge, ipcRenderer } from 'electron'
 import { electronAPI } from '@electron-toolkit/preload'
+import jsonObj from './api.json'
+import { TApi, TMainWindowMsg } from '@common/types'
 
 // Custom APIs for renderer
-const api = {}
 
-// Use `contextBridge` APIs to expose Electron APIs to
-// renderer only if context isolation is enabled, otherwise
-// just add to the DOM global.
+function createApi(): TApi {
+  const api: TApi = {
+    sendMainWindowMsg: (msg: TMainWindowMsg) => {
+      // ipcMain.on('main-window-msg', (ev, msg: TMainWindowMsg): void => {})
+      return ipcRenderer.send('main-window-msg', msg)
+    },
+    invokeReadJsonFile: (filename: string) => {
+      // ipcMain.handle('read-json-file', async (ev, filename: string): Promise<unknown> => {})
+      return ipcRenderer.invoke('read-json-file', filename)
+    }
+  }
+  for (const [serviceName, fnNameList] of Object.entries(jsonObj)) {
+    api[serviceName] = {}
+    for (const fnName of fnNameList) {
+      api[serviceName][fnName] = (...args: unknown[]) => {
+        return ipcRenderer.invoke(`${serviceName}.${fnName}`, ...args)
+      }
+    }
+  }
+  return api
+}
+
+// Use `contextBridge` APIs to expose Electron APIs to renderer only if context isolation is enabled,
+// otherwise just add to the DOM global.
 if (process.contextIsolated) {
   try {
     contextBridge.exposeInMainWorld('electron', electronAPI)
-    contextBridge.exposeInMainWorld('api', api)
+    contextBridge.exposeInMainWorld('api', createApi())
   } catch (error) {
     console.error(error)
   }
